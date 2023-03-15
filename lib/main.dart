f88653b11f764dcf356910a414e4a473ed98e572
Sherif Nafee <shico.m.nafee@gmail.com>
2023-03-15 19:25:42 +0300

    Update main for jokes screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:soar_quest/soar_quest.dart';
+
+import 'device_id.dart';
+import 'firebase/firebase_options.dart';
+import 'screens/fav_screen.dart';
+import 'screens/jokes_screen.dart';
+import 'screens/setting_screen.dart';
+
+late final SQCollection categories;
+
 void main() async {
+  await SQApp.init(
+    "Tinder with Chuck Norris",
+    theme: ThemeData(primarySwatch: Colors.deepOrange),
+    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
+  SQApp.run([
+    const JokesScreen("Jokes", icon: Icons.comment),
+    UserSettings.settingsScreen(),
+  ]);
 }
